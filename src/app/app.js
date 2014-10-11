b76f8c395a8de165bca1543c37a0923030ae7fc1
Paul Leclercq <paleclercq@gmail.com>
2014-10-11 19:59:52 +0200

    add anchors scroll, chronometer with laps time clear and so on

--- src/app/app.js
+++ src/app/app.js
@@ -3,7 +3,7 @@ angular.module( 'ngBoilerplate', [
   'templates-common',
   'ngBoilerplate.home',
   'ngBoilerplate.about',
-  'ui.router'
+  'ui.router',
 ])
 
 .config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
@@ -14,12 +14,23 @@ angular.module( 'ngBoilerplate', [
     amMoment.changeLocale('fr'); //momentJS
 })
 
-.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
+.controller( 'AppCtrl', function AppCtrl ( $scope, $location, $anchorScroll ) {
   $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
     if ( angular.isDefined( toState.data.pageTitle ) ) {
       $scope.pageTitle = toState.data.pageTitle + 'Reveil en ligne.fr' ;
     }
   });
+
+  //utils
+  //Go to anchor
+  $scope.goToAnchor = function(anchorName) {
+      // set the location.hash to the id of
+      // the element you wish to scroll to.
+      $location.hash(anchorName);
+
+      // call $anchorScroll()
+      $anchorScroll();
+  };
 })
 
 ;
