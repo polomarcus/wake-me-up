291a71e39033f67690f1b91242cdce3c66263aa1
Paul Leclercq <paleclercq@gmail.com>
2015-01-21 21:46:05 +0100

    add teads configuration

--- src/app/home/adsDirective.js
+++ src/app/home/adsDirective.js
@@ -5,8 +5,28 @@ app.directive('ads', function() {
     restrict: 'E',
     templateUrl: 'home/ads.tpl.html',
     link : function(scope, element, attrs) {
-      var ad = angular.element('<script src="http://www2.adserverpub.com/group.php?id=22019-27062&data=1&width=300&height=250"></script>');
-      element.append(ad);
+    //  var ad = angular.element('<script src="http://www2.adserverpub.com/group.php?id=22019-27062&data=1&width=300&height=250"></script>');
+    //  element.append(ad);
+          $(window).load(function() {
+            window._ttf = window._ttf || [];
+            _ttf.push({
+                pid          : 22738,
+                lang        : "fr",
+                slot        : 'live-data',
+                format      : "inread",
+                before      : true,
+                minSlot     : 0,
+                BTF         : false
+            });
+
+            (function (d) {
+                 var js, s = d.getElementsByTagName('script')[0];
+                 js = d.createElement('script');
+                 js.async = true;
+                 js.src = 'http://cdn.teads.tv/js/all-v1.js';
+                 s.parentNode.insertBefore(js, s);
+            })(window.document);
+          });
     }
   };
 });
