e915bf9939a0bf9766fa0bdf5cb6de4b2569c4c1
Paul Leclercq <paul.leclercq@teads.Tv>
2015-03-11 21:07:44 +0100

    remove comments

--- src/app/home/chronoCtrl.js
+++ src/app/home/chronoCtrl.js
@@ -70,7 +70,6 @@ angular.module( 'ChronoModule', [
         $scope.timerPaused = false;
         $scope.timerBegin = true;
         $scope.timerStartOrStop = 'CHRONO.BUTTON.START';
-        console.log(" $scope.timerStartOrStop",  $scope.timerStartOrStop);
     }
 
     init();
