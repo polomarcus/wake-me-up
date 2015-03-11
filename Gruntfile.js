9ec978e982c6fd0e8c2fd87a466fb0b3d7017902
Paul Leclercq <paul.leclercq@teads.Tv>
2015-03-11 22:55:50 +0100

    gruntfile, e2e after default grunt, do not open chrome when testing bin folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -96,11 +96,7 @@ module.exports = function ( grunt ) {
         options: {
             port: 8080,
             base: 'bin',
-            hostname: 'localhost',
-            open : {
-                target: 'http://localhost:8080', // target url to open
-                appName: 'Google Chrome' // name of the app that opens, ie: open, start, xdg-open
-            }
+            hostname: 'localhost'
         }
       },
       prod: {
@@ -659,7 +655,7 @@ module.exports = function ( grunt ) {
   /**
    * The default task is to build and compile.
    */
-  grunt.registerTask( 'default', [ 'build', 'compile'] );
+  grunt.registerTask( 'default', [ 'build', 'compile', 'e2e'] );
 
   //Staging FTP
   grunt.registerTask( 'staging', ['ftp-deploy:build' ] );
