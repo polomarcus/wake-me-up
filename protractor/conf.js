f8b57bc54f0d9a475a24c3d7848f134429633772
Paul Leclercq <paul.leclercq@teads.Tv>
2015-03-15 23:27:58 +0100

    remove useless comments

--- protractor/conf.js
+++ protractor/conf.js
@@ -3,7 +3,6 @@ exports.config =  {
     allScriptsTimeout: 11000,
     specs: ['../src/test/e2e/*.js'],
     baseUrl: 'http://localhost:4444/wd/hub',
-    //baseUrl: 'http://localhost:8080/',
     maxSessions: 1,
     multiCapabilities: [{ //You can add several browsers here
         'browserName': 'chrome'
