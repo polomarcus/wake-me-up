168caaa4bcf2055ae23715a56645a328be334a6c
Paul Leclercq <paul.leclercq@teads.Tv>
2015-04-13 23:12:47 +0200

    chores(GA): fix GA

--- src/assets/js/googleTrack.js
+++ src/assets/js/googleTrack.js
@@ -1,9 +1,14 @@
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-27105689-1']);
-_gaq.push(['_trackPageview']);
+if(typeof _gaq === "undefined"){
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-27105689-1']);
+  _gaq.push(['_trackPageview']);
 
-(function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  (function () {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+  })();
+}
