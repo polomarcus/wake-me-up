514b4415e44337733df18b5f74491a1863fb1cd7
Paul Leclercq <paul.leclercq@teads.Tv>
2015-04-24 18:37:51 +0200

    chores(GA): other safe call

--- src/common/analytics/gaTracker.js
+++ src/common/analytics/gaTracker.js
@@ -3,16 +3,17 @@ angular.module('wakeMeUp')
     .config(function ($provide) {
         $provide.decorator("$exceptionHandler", function ($delegate) {
             return function (exception, cause) {
-                var _gaq = _gaq || []; //safe script loading for GA
                 $delegate(exception, cause);
-                _gaq.push([
-                    '_trackEvent',
-                    'AngularJS error',
-                    exception.message,
-                    exception.stack,
-                    0,
-                    true
-                ]);
+                if(typeof _gaq !== "undefined") {
+                    _gaq.push([
+                        '_trackEvent',
+                        'AngularJS error',
+                        exception.message,
+                        exception.stack,
+                        0,
+                        true
+                    ]);
+                }
             };
         });
     });
--- src/common/analytics/gaTrackerService.js
+++ src/common/analytics/gaTrackerService.js
@@ -3,15 +3,15 @@ var app = angular.module('wakeMeUp.services');
 app.factory('gaTrackerService', function($log) {
   var GaTrackerService = function() {};
 
-  var _gaq = _gaq || []; //safe script loading for GA
-
   GaTrackerService.prototype.track = function track(category, type, message){
-    _gaq.push([
-      '_trackEvent',
-      category,
-      type,
-      message
-    ]);
+    if(typeof _gaq !== "undefined"){
+      _gaq.push([
+        '_trackEvent',
+        category,
+        type,
+        message
+      ]);
+    }
   };
 
   return new GaTrackerService();
