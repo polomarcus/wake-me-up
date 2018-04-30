4a65d34cf2accbdd49410bf583a7c056c8e5088a
Paul Leclercq <paleclercq@gmail.com>
2018-04-30 13:44:02 -0400

    test: time helper

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {calculateRemainingTime,isItTime, launchAlarm} from './helper/time.js';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('test time helper', () => {
+  const currentTime = new Date('December 17, 1995 03:24:00');
+  const futureDate = new Date('December 17, 1995 04:25:00');
+
+  expect(calculateRemainingTime(futureDate, currentTime)).toEqual({"hours": 1, "minutes": 1})
+
+  expect(isItTime(futureDate, currentTime)).toEqual(false)
+  expect(isItTime(currentTime, currentTime)).toEqual(true)
 });
